34db3b7a137feb7c84f471faf432c353f654518b
hubibrahim <ibrahimabdou69196@gmail.com>
2023-11-20 10:40:35 +0100

    Nouvelle route /groups Modification contacts.js du  commentaire

--- routes/groups.js
+++ routes/groups.js
@@ -1,9 +1,1 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET groups listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+Holla groups
